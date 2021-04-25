45a67c718f65b2fb7ba2a184526cd434c3b2ce31
davenportLThomas <thomas.davenport@wsu.edu>
2021-04-24 20:47:35 -0700

    Added pickup() and started run

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ class Philosopher
 	int id;
 	string name;
 	Chopstick *left, *right;
+	//the status of how hungry dude is, at 10 hungry he dies, is death in the requirements
+	int hungry = 0;//I should randomize
 
 	Philosopher(string _name, Chopstick *_left, Chopstick *_right)
 	{
@@ -46,9 +48,51 @@ class Philosopher
 		//set some chopsticks, gotta eat
 		this->left = _left;
 		this->right = _right;
-	}	
+	}
 
-	
+	//pickUp is what is to be used to pick up chopsticks. It needs to return 0 
+	bool pickUp()
+	{
+		//try to pick up
+		if(left->state == left->UNUSED)//can be picked up
+		{
+			if(right->state == right->UNUSED)//picked up both!
+			{
+				left->state = left->USED;
+				right->state = right->USED;
+				return 1;
+			}
+			else
+				return 0;
+
+		}
+		else return 0;
+
+	}
+
+	void release()//I suppose all releases should be successful
+	{
+		
+
+
+	}
+
+
+	void run()//this is going to be the philosophical loop if you will
+	{
+		do
+		{
+			if(pickUp())//returns true if it can be picked up
+			{
+
+			}
+			else//pickup did not happen
+			{
+
+			}
+
+		}while(1);
+	}	
 };
 
 
